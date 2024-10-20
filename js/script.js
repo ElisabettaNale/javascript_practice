9451f94c6f068a3ee7716c9f86abe9b607aedc84
ElisabettaNale <elisabetta.nale@gmail.com>
2024-10-20 15:13:43 +0200

    improve functions, move style to css, improve mobile layout

--- js/script.js
+++ js/script.js
@@ -1,45 +1,19 @@
 ///////////////////////////////////////////////////////////////
-// Funzioni   
+// Funzioni
 ///////////////////////////////////////////////////////////////
 
-// Funzione per creare un pulsante 
-function createButton(text, styles) {
-    let button = document.createElement('button');
-    button.textContent = text;
-    button.style.margin = "5px";
-    button.style.height = styles["height"];
-    button.style.width = styles["width"];
-    button.style.fontSize = styles["fontSize"];
-    button.style.fontWeight = "bold";
-    button.style.border = styles["border"];
-    button.style.borderRadius = "10%";
-    button.style.backgroundColor = styles["backgroundColor"];
-    button.style.color = "#465038";
-    button.style.cursor = "pointer";
-    button.style.transition = styles["transition"];
-    return button;
-}
+// Funzione per creare gli elementi del DOM
+function createElement(type, css_class, text = '') {
+    
+    let element = document.createElement(type);
 
-// Funzione per gestire gli eventi di click sui pulsanti
-function handleButtonClick(event) {
-    if (event.target === plusButton) {
-        number.textContent++;
-    } else if (event.target === minusButton) {
-        number.textContent--;
-    } else if (event.target === resetButton) {
-        number.textContent = 0;
-    }
-}
+    element.classList.add(css_class)
 
-// Funzione per gestire l'hover sui pulsanti
-function handleHover(event, hover) {
-    let target = event.target;
-    if (target === plusButton || target === minusButton) {
-        target.style.transform = hover ? 'scale(0.9)' : 'scale(1)';
-    } else if (target === resetButton) {
-        resetButton.style.backgroundColor = hover ? "#fbc8a2" : "#f9ddad";
-        resetButton.style.fontSize = hover ? "20px" : "18px";
+    if (text) {
+        element.textContent = text;
     }
+
+    return element;
 }
 
 
@@ -47,47 +21,25 @@ function handleHover(event, hover) {
 // STRUTTURA DOM   
 ///////////////////////////////////////////////////////////////
 
-// Recupera counter
+// Inizializza il counter
 let counter = document.querySelector(".counter");
 
-// Crea, inizializza e aggiungi number
-let number = document.createElement("div");
-number.textContent = 0;
-number.style.marginBottom = "40px";
-number.style.fontSize = "50px";
+// Crea il numero
+let number = createElement("div", "number", "0");
 counter.appendChild(number);
 
-// Crea, inizializza e aggiungi i pulsanti +/-
-let buttons = document.createElement("div");
-buttons.style.display = "flex";
-buttons.flexDirection = "row";
-buttons.marginBottom = "20px";
-let plusButton = createButton("+", {height: "50px",
-                                    width: "70px",
-                                    fontSize: "28px",
-                                    border: "#fbc8a2 3px solid",
-                                    backgroundColor: "#fbc8a2",
-                                    transition: "0.5s"}
-)
-let minusButton = createButton("-", {height: "50px",
-                                     width: "70px",
-                                     fontSize: "28px",
-                                     border: "#fbc8a2 3px solid",
-                                     backgroundColor: "#fbc8a2",
-                                     transition: "0.5s"}
-)
-counter.appendChild(buttons);
-buttons.appendChild(plusButton);
-buttons.appendChild(minusButton);
+// Crea il contenitore per i pulsanti
+let buttonsContainer = createElement("div", "buttons", "");
+counter.appendChild(buttonsContainer);
 
-// Crea, inizializza e aggiungi il pulsante reset
-let resetButton = createButton("RESET", {height: "40px",
-                                         width: "100px",
-                                         fontSize: "18px",
-                                         border: "#fbc8a2 1px solid",
-                                         backgroundColor: "#f9ddad",
-                                         transition: "0.8s"}
-)
+// Crea i pulsanti +/- 
+let plusButton = createElement("button", "button", "+");
+let minusButton = createElement("button", "button", "-");
+buttonsContainer.appendChild(plusButton);
+buttonsContainer.appendChild(minusButton);
+
+// Crea il pulsante reset 
+let resetButton = createElement("button", "button-reset", "RESET");
 counter.appendChild(resetButton);
 
 
@@ -95,7 +47,53 @@ counter.appendChild(resetButton);
 // EVENTI    
 ///////////////////////////////////////////////////////////////
 
-// Event listeners per click, mouseover/mouseout
-counter.addEventListener('click', handleButtonClick);
-counter.addEventListener('mouseover', (event) => handleHover(event, true));
-counter.addEventListener('mouseout', (event) => handleHover(event, false));
+// Funzione per gestire gli eventi sui pulsanti
+function handleEvent(event) {
+
+    let target = event.target;
+
+    // click
+    if (event.type === 'click') {
+        if (target === plusButton) {
+            number.textContent++;
+        } else if (target === minusButton) {
+            number.textContent--;
+        } else if (target === resetButton) {
+            number.textContent = 0;
+        }
+    }
+
+    // mouseover/mouseout o touchstart/touchend
+    else if (event.type === 'mouseover' || 
+            event.type === 'mouseout' ||
+            event.type === 'touchstart' ||
+            event.type === 'touchend') {
+
+        let isHover = (event.type === 'mouseover' || event.type === 'touchstart');
+
+        if (isHover) {
+
+            if (target === plusButton || target === minusButton) {
+                target.classList.add('button-hover');
+            } else if (target === resetButton) {
+                target.classList.add('button-reset-hover')
+            }
+
+        } else {
+
+            if (target === plusButton || target === minusButton) {
+                target.classList.remove('button-hover');
+            } else if (target === resetButton) {
+                target.classList.remove('button-reset-hover')
+            }
+
+        }
+    }
+}
+
+// Event listeners per click, mouseover/mouseout, touchstart/toouchend
+counter.addEventListener('click', handleEvent);
+counter.addEventListener('mouseover', handleEvent);
+counter.addEventListener('mouseout', handleEvent);
+counter.addEventListener('touchstart', handleEvent);
+counter.addEventListener('touchend', handleEvent);
